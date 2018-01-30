a33fdd14727a98dd35c3fb8896171ff472d4d72e
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-01-30 14:23:00 -0800

    took out default values in scraper

--- lib/bookworm_buddy/scraper.rb
+++ lib/bookworm_buddy/scraper.rb
@@ -1,6 +1,7 @@
 require 'pry'
 class BookwormBuddy::Scraper
     @@categories = []
+    @@bestsellers = []
     
     def self.list_categories
         doc = Nokogiri::HTML(open("https://www.barnesandnoble.com/b/books/_/N-1fZ29Z8q8?&page=1&showMoreIds=10008"))
@@ -12,20 +13,22 @@ class BookwormBuddy::Scraper
             end
         @@categories.slice!(-1)
         @@categories.each_with_index {|category, index| puts "#{index+1}. #{category[:name]}"}
-        list_books_by_category("4")
+        list_books_by_category("7")
     end
 
-    def self.list_books_by_category(category_number = "1")
-        bestseller_url = @@categories["1".to_i - 1] && @@categories["1".to_i - 1][:url]
+    def self.list_books_by_category(category_number)
+        bestseller_url = @@categories[category_number.to_i - 1] && @@categories[category_number.to_i - 1][:url]
         doc = Nokogiri::HTML(open(bestseller_url))
+        
             doc.css('div.col-lg-8.product-info-listView').each do |book|
-            attributes_hash = {}
+                attributes_hash = {}
                 attributes_hash[:title] = book.css('h3.product-info-title a').text
                 attributes_hash[:author] = book.css('div.product-shelf-author.contributors a').first.text
-                attributes_hash[:price] = book.css("span.current a").text
+                attributes_hash[:price] = book.css("span.current a").first.text
                 attributes_hash[:description_url] = "https://www.barnesandnoble.com#{book.css('h3.product-info-title a').attr('href').value}"
+                @@bestsellers << attributes_hash
             end
-        #must grab title, author, price & url 
+       @@bestsellers.each_with_index {|book, index| puts "#{index+1}. #{book[:title]} - by: #{book[:author]} - #{book[:price]}"} 
     end
 
     def self.book_description
