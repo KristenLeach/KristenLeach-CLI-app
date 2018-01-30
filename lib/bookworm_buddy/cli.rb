7a0d30db2e8532c08b0e90d7e85f331b0abcdd4d
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-01-30 10:57:04 -0800

    added to cli

--- lib/bookworm_buddy/cli.rb
+++ lib/bookworm_buddy/cli.rb
@@ -1,10 +1,49 @@
 class BookwormBuddy::CLI
 
     def call
-        list
+        puts "Welcome to the Bookworm Buddy!"
+        puts "\n""We bring you the top 20 Barnes & Noble bestselling books in the category of your choice, so you'll have fresh material in no time!"
+        puts "\n""To see the list of categories, enter 'list'."
+        input
+    end
+
+    def input
+        input = gets.strip
+        if input != input.to_i
+            case input 
+            when "list"
+                list
+            when "exit"
+                exit
+            end
+        elsif input.to_i.between?(1, 50)
+            bestsellers
+        else 
+            "I'm not sure what you meant. Please try again."
+            input
+        end
     end
 
     def list
         BookwormBuddy::Scraper.list_categories
+        puts "----------------------------------------"
+        puts "To see the top 20 bestsellers in a category, enter the category number below."
+        puts "----------------------------------------"
+        input
+        #bestsellers
+    end
+
+    def bestsellers
+        #input = gets.to_i
+        #if input.between?(1, 50)
+            puts "this is working"
+       # else 
+           # puts "... I am not sure what you meant. Please try again."
+        #end
+    end
+
+    def exit
+        puts "We hope you found what you were looking for!"
+        puts "Happy reading."
     end
 end
--- lib/bookworm_buddy/scraper.rb
+++ lib/bookworm_buddy/scraper.rb
@@ -1,17 +1,28 @@
 require 'pry'
 class BookwormBuddy::Scraper
-
+    @@categories = []
+    
     def self.list_categories
         doc = Nokogiri::HTML(open("https://www.barnesandnoble.com/b/books/_/N-1fZ29Z8q8?&page=1&showMoreIds=10008"))
-        categories = []
             doc.css("ul#sidebar-section-0 li").collect do |link| 
             category_hash = {}
                 category_hash[:name] = link.text.strip
                 category_hash[:url] = link.css('a.bread-crumbs__item').attr('href').value
-                categories << category_hash 
+                @@categories << category_hash 
             end
-        categories.slice!(-1)
-        categories.each_with_index {|category, index| puts "#{index+1}. #{category[:name]}"}
-        binding.pry
+        @@categories.slice!(-1)
+        @@categories.each_with_index {|category, index| puts "#{index+1}. #{category[:name]}"}
+        #binding.pry
     end
+
+    def self.list_books_by_category(category_number)
+        bestseller_url = @@categories[category_number.to_i - 1] && @@categories[category_number.to_i - 1][:url]
+        doc = Nokogiri::HTML(open(bestseller_url))
+        #must grab title, author, price & url 
+    end
+
+    def self.book_description
+        #must grab short description of book
+    end
+
 end
