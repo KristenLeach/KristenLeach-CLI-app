88f0c43cd08b91f4329c144928f71bb66a6fbf1c
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-02-07 21:36:42 -0800

    finished book object

--- lib/bookworm_buddy/book.rb
+++ lib/bookworm_buddy/book.rb
@@ -1,19 +1,25 @@
 #arrays of books from each category, & description?
+require 'pry'
 class BookwormBuddy::Book
-    attr_accessor :title, :author, :price, :description
+    attr_accessor :title, :author, :price, :description, :description_url
+    @@all = []
 
     #initialized with attribute hash 
-    def initialize(attribute_hash)
+    def initialize(bestseller_hash)
         bestseller_hash.each {|key, value| self.send("#{key}=", value)}
         @@all << self 
+        #binding.pry
     end
 
     def self.create(book_array)
-
+        book_array.each {|book| self.new(book)}
     end
 
-    def list_books_by_category(category_number)
-        @@bestsellers.each_with_index {|book, index| puts "#{index+1}. #{book[:title]} - by: #{book[:author]} - #{book[:price]}"} 
+    def self.list_books_by_category(category_number)
+        @@all.each_with_index {|book, index| puts "#{index+1}. \"#{book.title}\" - by: #{book.author} - #{book.price}"} 
     end
     
+    def self.add_description(description)
+        @description << description
+    end
 end
